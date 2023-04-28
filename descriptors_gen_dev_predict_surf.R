5c93205056e7fdd6ad577c5edc08f5f6af8c8086
amkrajewski <ak@psu.edu>
2023-04-28 15:07:21 -0400

    - minor style improvements

--- descriptors_gen_dev_predict_surf.R
+++ descriptors_gen_dev_predict_surf.R
@@ -1,43 +1,43 @@
-des_surf <- function (comp=comp){
-  comp_n <- comp/sum(comp)
+des_surf <- function(comp = comp) {
+  comp_n <- comp / sum(comp)
   a <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
   data.1 <- data.frame(rbind(a, comp_n))
-  
+
   file.3 <- "elemental_features.csv"
-  data.3 <- read.csv(file=file.3, header=T)
+  data.3 <- read.csv(file = file.3, header = T)
   para_ele <- as.matrix(data.3[, 2:4])
-  
-  load(file="features_surf.Rdata")
-  
+
+  load(file = "features_surf.Rdata")
+
   feature.surf.select <- feature.surf.all
   n.ob <- length(data.1[, 1])
   n.feature <- length(feature.surf.select[1, 1,])
   n.feature.ele <- length(para_ele[1,])
-  n.feature.all <- n.feature+n.feature.ele
-  
+  n.feature.all <- n.feature + n.feature.ele
+
   parameter.surf <- array(0, c(n.ob, 10, n.feature))
-  
+
   comp_ele <- as.matrix(data.1)
-  for (i in seq(n.feature)){
-    parameter.surf[,,i] <- comp_ele%*%as.matrix(feature.surf.select[,, i])
+  for (i in seq(n.feature)) {
+    parameter.surf[, , i] <- comp_ele %*% as.matrix(feature.surf.select[, , i])
   }
 
-  size <- 2*n.feature.all
+  size <- 2 * n.feature.all
   predictors <- array(0, c(n.ob, size))
-  for (i in seq(n.ob)){
-    nonzvec <- which(comp_ele[i,]!=0)
+  for (i in seq(n.ob)) {
+    nonzvec <- which(comp_ele[i,] != 0)
     avg1 <- NULL
-    for (k in seq(n.feature)){
+    for (k in seq(n.feature)) {
       para.temp <- parameter.surf[i, nonzvec, k]
-      avgtemp <- para.temp%*%comp_ele[i, nonzvec]
-      dvetemp <- ((1/(1-sum(comp_ele[i, nonzvec]^2)))*((para.temp-avgtemp[1])^2%*%comp_ele[i, nonzvec]))^(1/2)
+      avgtemp <- para.temp %*% comp_ele[i, nonzvec]
+      dvetemp <- ((1 / (1 - sum(comp_ele[i, nonzvec]^2))) * ((para.temp - avgtemp[1])^2 %*% comp_ele[i, nonzvec]))^(1 / 2)
       avg1 <- c(avg1, avgtemp, dvetemp)
     }
 
     avg2 <- NULL
-    for (n in seq(n.feature.ele)){
-      avgtemp <- comp_ele[i, nonzvec]%*%para_ele[nonzvec, n]
-      dvetemp <- ((1/(1-sum(comp_ele[i, nonzvec]^2)))*((para_ele[nonzvec, n]-avgtemp[1])^2%*%comp_ele[i, nonzvec]))^(1/2)
+    for (n in seq(n.feature.ele)) {
+      avgtemp <- comp_ele[i, nonzvec] %*% para_ele[nonzvec, n]
+      dvetemp <- ((1 / (1 - sum(comp_ele[i, nonzvec]^2))) * ((para_ele[nonzvec, n] - avgtemp[1])^2 %*% comp_ele[i, nonzvec]))^(1 / 2)
       avg2 <- c(avg2, avgtemp, dvetemp)
     }
     avg <- c(avg1, avg2)
