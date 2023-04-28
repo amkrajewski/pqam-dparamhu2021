a6c6c0b84f373c05d53cd522d1fdff94cbfa638b
amkrajewski <ak@psu.edu>
2023-04-28 15:02:25 -0400

    - where applicable, changed '=' to ' <- ' assignment

--- HEA_pred.R
+++ HEA_pred.R
@@ -8,15 +8,15 @@ HEA_pred <- function (composition = composition){
   load("locfit_surf_2_var_all.Rdata")
   model_surf <- model
 
-  descriptors.gsf=des_gsf(comp=composition)
-  descriptors.surf=des_surf(comp=composition)
-  sel=c(1:26,35:38)
-  descriptors.gsf=as.matrix(descriptors.gsf[,sel])
-  descriptors.surf=as.matrix(descriptors.surf[,sel])
-  pre.gsf=predict.gbm.locfit(model_gsf, descriptors.gsf)
-  pre.surf=predict.gbm.locfit(model_surf, descriptors.surf)
-  D=pre.surf[2]/pre.gsf[2]
-  result=t(as.matrix(c(pre.gsf[2],pre.surf[2],D)))
+  descriptors.gsf <- des_gsf(comp=composition)
+  descriptors.surf <- des_surf(comp=composition)
+  sel <- c(1:26, 35:38)
+  descriptors.gsf <- as.matrix(descriptors.gsf[, sel])
+  descriptors.surf <- as.matrix(descriptors.surf[, sel])
+  pre.gsf <- predict.gbm.locfit(model_gsf, descriptors.gsf)
+  pre.surf <- predict.gbm.locfit(model_surf, descriptors.surf)
+  D <- pre.surf[2]/pre.gsf[2]
+  result <- t(as.matrix(c(pre.gsf[2], pre.surf[2], D)))
   colnames(result) <- c("GSF","Surface","D parameter")
   return(result)
 }
--- descriptors_gen_dev_predict_gsf.R
+++ descriptors_gen_dev_predict_gsf.R
@@ -1,54 +1,54 @@
 des_gsf <- function(comp=comp){
-  comp_n=comp/sum(comp)
-  a=c(1,1,1,1,1,1,1,1,1,1)
-  data.1=data.frame(rbind(a,comp_n))
+  comp_n <- comp/sum(comp)
+  a <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+  data.1 <- data.frame(rbind(a, comp_n))
   
-  file.3="elemental_features.csv"
-  data.3=read.csv(file=file.3,header=T)
-  para_ele=as.matrix(data.3[,2:4])
+  file.3 <- "elemental_features.csv"
+  data.3 <- read.csv(file=file.3, header=T)
+  para_ele <- as.matrix(data.3[, 2:4])
 
   load(file="features_gsf.Rdata")
   
-  feature.gsf.select=feature.gsf.all
-  n.ob=length(data.1[,1])
-  n.feature=length(feature.gsf.select[1,1,])
-  n.feature.ele=length(para_ele[1,])
-  n.feature.all=n.feature+n.feature.ele
+  feature.gsf.select <- feature.gsf.all
+  n.ob <- length(data.1[, 1])
+  n.feature <- length(feature.gsf.select[1, 1,])
+  n.feature.ele <- length(para_ele[1,])
+  n.feature.all <- n.feature+n.feature.ele
   
-  parameter.gsf=array(0,c(n.ob,10,n.feature))
+  parameter.gsf <- array(0, c(n.ob, 10, n.feature))
   
-  comp_ele=as.matrix(data.1)
+  comp_ele <- as.matrix(data.1)
   for (i in seq(n.feature)){
-    parameter.gsf[,,i]=comp_ele%*%as.matrix(feature.gsf.select[,,i])
+    parameter.gsf[,,i] <- comp_ele%*%as.matrix(feature.gsf.select[,, i])
   }
   
-  order=c(1)
-  size=2*n.feature.all
-  predictors=array(0,c(n.ob,size))
+  order <- c(1)
+  size <- 2*n.feature.all
+  predictors <- array(0, c(n.ob, size))
   for (i in seq(n.ob)){
-    avg=NULL
-    nonzvec = which(comp_ele[i,]!=0)
-    avg1=NULL
+    avg <- NULL
+    nonzvec <- which(comp_ele[i,]!=0)
+    avg1 <- NULL
     for (k in seq(n.feature)){
-      para.temp=parameter.gsf[i,nonzvec,k]
-      avgtemp=para.temp%*%comp_ele[i,nonzvec]
-      dvetemp=((1/(1-sum(comp_ele[i,nonzvec]^2)))*((para.temp-avgtemp[1])^2%*%comp_ele[i,nonzvec]))^(1/2)
-      avg1=c(avg1,avgtemp,dvetemp)
+      para.temp <- parameter.gsf[i, nonzvec, k]
+      avgtemp <- para.temp%*%comp_ele[i, nonzvec]
+      dvetemp <- ((1/(1-sum(comp_ele[i, nonzvec]^2)))*((para.temp-avgtemp[1])^2%*%comp_ele[i, nonzvec]))^(1/2)
+      avg1 <- c(avg1, avgtemp, dvetemp)
     }
 
-    avg2=NULL
+    avg2 <- NULL
     for (n in seq(n.feature.ele)){
-      avgtemp=comp_ele[i,nonzvec]%*%para_ele[nonzvec,n]
-      dvetemp=((1/(1-sum(comp_ele[i,nonzvec]^2)))*((para_ele[nonzvec,n]-avgtemp[1])^2%*%comp_ele[i,nonzvec]))^(1/2)
-      avg2=c(avg2,avgtemp,dvetemp)
+      avgtemp <- comp_ele[i, nonzvec]%*%para_ele[nonzvec, n]
+      dvetemp <- ((1/(1-sum(comp_ele[i, nonzvec]^2)))*((para_ele[nonzvec, n]-avgtemp[1])^2%*%comp_ele[i, nonzvec]))^(1/2)
+      avg2 <- c(avg2, avgtemp, dvetemp)
     }
-    avg=c(avg1,avg2)
-    predictors[i,]=avg
+    avg <- c(avg1, avg2)
+    predictors[i,] <- avg
   }
   
   
-  all=data.frame(predictors)
-  all[is.na(all)]=0
+  all <- data.frame(predictors)
+  all[is.na(all)] <- 0
   return(all)
 }
 
