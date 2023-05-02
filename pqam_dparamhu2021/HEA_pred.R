3154a0b9c6f6fea26953aecc845d111bf84af939
amkrajewski <ak@psu.edu>
2023-05-02 12:22:36 -0400

    - moved the model initialization outside the predictive function. It should be much faster now.

--- pqam_dparamhu2021/HEA_pred.R
+++ pqam_dparamhu2021/HEA_pred.R
@@ -1,13 +1,15 @@
-HEA_pred <- function(composition = composition, path) {
+init <- function (path) {
   source(paste(path,"/descriptors_gen_dev_predict_surf.R", sep = ""))
   source(paste(path,"/descriptors_gen_dev_predict_gsf.R", sep = ""))
   source(paste(path,"/gbm-locfit.R", sep = ""))
-
   load(paste(path,"/locfit_3_var_all.Rdata", sep = ""))
-  model_gsf <- model
+  model_gsf <<- model
   load(paste(path,"/locfit_surf_2_var_all.Rdata", sep = ""))
-  model_surf <- model
+  model_surf <<- model
+  return('init done')
+}
 
+HEA_pred <- function(composition = composition, path) {
   descriptors.gsf <- des_gsf(comp = composition, path = path)
   descriptors.surf <- des_surf(comp = composition, path = path)
   sel <- c(1:26, 35:38)
